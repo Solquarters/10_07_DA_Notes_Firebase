4145384ca4567c38b8957829f6c0c8c36b07c4c0
Solquarters <rom.kabucov@gmail.com>
2024-10-07 21:59:34 +0200

    service connection to firestore added collection and singleDoc getter with injectable and all imports

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+
+  firestore: Firestore = inject(Firestore);
+  constructor() {
+
+   }
+   // const itemCollection = collection(this.firestore, 'items');
+   getNotesRef(){
+    return collection(this.firestore, 'notes');
+   }
+
+   getTrashRef(){
+    return collection(this.firestore, 'trash');
+   }
+
+   getSingleDocRef(collectionId: string, docId: string){
+ return doc(collection(this.firestore, collectionId), docId);
+   }
 }
